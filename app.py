eb636e6d104ccb31902f695c8605b1ea7c575a56
AICraftAlchemy <aicraftalchemy@gmail.com>
2024-09-21 16:09:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key = os.urandom(24)
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = ' '
-MYSQL_DB = 'login'
+MYSQL_DB = ' '
 
 # File to store login and logout details
 LOG_FILE = 'user_activity_logs.xlsx'
